4fdb6af809e657a9766709442649288fdc1975e2
willbouch <williambouchardpei@gmail.com>
2019-10-01 13:02:07 -0400

    Initial commit of the backend application

---
+++ TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/TutoringService321Application.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.tutoringservice321;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class TutoringService321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringService321Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello";
+	}
+}
---
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/TutoringService321ApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.tutoringservice321;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutoringService321ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
