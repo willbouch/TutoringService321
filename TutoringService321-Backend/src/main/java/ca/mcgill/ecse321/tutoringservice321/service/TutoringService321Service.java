6ab22550a6910a7e6ce66754c63b97963bd7b6c9
willbouch <46496014+willbouch@users.noreply.github.com>
2019-10-12 16:41:20 -0400

    Template to start coding the service class

--- TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/service/TutoringService321Service.java
+++ TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/service/TutoringService321Service.java
@@ -11,55 +11,5 @@ import org.springframework.transaction.annotation.Transactional;
 import ca.mcgill.ecse321.tutoringservice321.dao.*;
 import ca.mcgill.ecse321.tutoringservice321.model.*;
 public class TutoringService321Service {
-
-	@Autowired
-	TutorRepository tutorRepository;
-	@Autowired
-	AvailabilityRepository availabilityRepository;
-	@Autowired
-	SessionRepository sessionRepository;
-	@Autowired
-	CourseRepository courseRepository;
-	@Autowired
-	SubjectRepository subjectRepository;
-	@Autowired
-	StudentRepository studentRepository;
 	
-	
-	@Transactional
-	public Availability createAvailability(Tutor tutor, Date date, Time startTime, Time endTime, int availabilityID) {
-		Availability a = new Availability();
-		a.setTutor(tutor);
-		a.setDate(date);
-		a.setStartTime(startTime);
-		a.setEndTime(endTime);
-		a.setAvailabilityID(availabilityID);
-		availabilityRepository.save(a);
-		return a;
-	}
-
-	@Transactional
-	public Optional<Availability> getAvailability(Integer ID) {
-		Optional<Availability> a=availabilityRepository.findById(ID);
-		return a;
-	}
-
-	@Transactional
-	public Course createCourse(String description, String school, String courseCode, int courseID,
-			Set<Subject> subjects) {
-		Course c = new Course();
-		c.setDescription(description);
-		c.setSchool(school);
-		c.setCourseCode(courseCode);
-		c.setCourseID(courseID);
-		c.setSubject(subjects);
-		courseRepository.save(c);
-		return c;
-	}
-
-	@Transactional
-	public Optional<Course> getCourse(Integer ID) {
-		Optional<Course> c=courseRepository.findById(ID);
-		return c;
-	}
 }
