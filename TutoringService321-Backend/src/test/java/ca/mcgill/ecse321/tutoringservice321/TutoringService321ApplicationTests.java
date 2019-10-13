9e0f0e5054e936169e5e9597eba40622e71c0656
sharonkat <sharon.kattar@mail.mcgill.ca>
2019-10-12 23:27:30 -0400

    First round of persistence test -- not all pass

--- TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/TutoringService321ApplicationTests.java
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/TutoringService321ApplicationTests.java
@@ -28,9 +28,7 @@ import ca.mcgill.ecse321.tutoringservice321.dao.StudentRepository;
 import ca.mcgill.ecse321.tutoringservice321.dao.SubjectRepository;
 import ca.mcgill.ecse321.tutoringservice321.dao.TutoringService321Repository;
 import ca.mcgill.ecse321.tutoringservice321.service.TutoringService321Service;
-import ca.mcgill.ecse321.tutoringservice321.model.Availability;
-import ca.mcgill.ecse321.tutoringservice321.model.Subject;
-import ca.mcgill.ecse321.tutoringservice321.model.Tutor;
+import ca.mcgill.ecse321.tutoringservice321.model.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -61,68 +59,174 @@ public class TutoringService321ApplicationTests {
 	}
 
 	@Test
-
+	// Failed
 	public void testWriteAvailability() {
+		assertEquals(0, service.getAllAvailabilities());
+		
+		String tutorEmail = "hadi@gmail.com";
+		
+		Calendar c = Calendar.getInstance();
+		c.set(2019, c.DECEMBER, 01, 12, 00);
+		Date date = new Date(c.getTimeInMillis());
+		Time startTime = new Time(c.getTimeInMillis());
+		c.set(16, 00);
+		Time endTime = new Time(c.getTimeInMillis());
+		
+		try {
+			service.createAvailability(tutorEmail, date, startTime, endTime);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List <Availability> allAvailabilities = service.getAllAvailabilities();
+	
+		assertEquals(1, allAvailabilities.size());
+		assertEquals(startTime, allAvailabilities.get(0).getStartTime());
 
 	}
 
 	@Test
-
+	// Failed
 	public void testViewAvailability() {
+		assertEquals(0, service.getAllAvailabilities());
+		
+		String tutorEmail = "hadi@gmail.com";
+		
+		Calendar c = Calendar.getInstance();
+		c.set(2019, c.DECEMBER, 01, 12, 00);
+		Date date = new Date(c.getTimeInMillis());
+		Time startTime = new Time(c.getTimeInMillis());
+		c.set(16, 00);
+		Time endTime = new Time(c.getTimeInMillis());
+		
+		try {
+			service.createAvailability(tutorEmail, date, startTime, endTime);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List <Availability> allAvailabilities = service.getAllAvailabilities();
+	
+		assertEquals(1, allAvailabilities.size());
+		assertEquals(startTime, allAvailabilities.get(0).getStartTime());
+		
 	}
 
 	@Test
-
+	// Failed
 	public void testWriteCourse(){
+		assertEquals(0, service.getAllCourses());
+		
+		String description = "This is a course";
+		String school = "McGill";
+		String courseCode = "ECSE321";
+		
+		try {
+			service.createCourse(description, school, courseCode);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List <Course> allCourses = service.getAllCourses();
+		
+		assertEquals(1, allCourses.size());
+		assertEquals(school, allCourses.get(0).getSchool());
 
 	}
 
 	@Test
-
+	// Failed
 	public void testViewCourse(){
-
+		assertEquals(0, service.getAllCourses());
+		
+		String description = "This is a course";
+		String school = "McGill";
+		String courseCode = "ECSE321";
+		
+		try {
+			service.createCourse(description, school, courseCode);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List <Course> allCourses = service.getAllCourses();
+		
+		assertEquals(1, allCourses.size());
+		assertEquals(school, allCourses.get(0).getSchool());
 	}
 
-	// ... other tests
-
 	@Test
-
-	public void testWriteServiceUser(){
-
-	}
-
-	@Test
-
-	public void testViewServiceUser(){
-
-	}
-
-	@Test
-
+	// Error
 	public void testWriteSession() {
-
+		assertEquals(0, service.getAllSessions().size());
+		
+		Calendar c = Calendar.getInstance();
+		c.set(2020,  Calendar.JANUARY, 10, 10, 00);
+		Date date = new Date(c.getTimeInMillis());
+		Time startTime = new Time(c.getTimeInMillis());
+		c.set(18, 00);
+		Time endTime = new Time(c.getTimeInMillis());
+		
+		String tutorEmail = "william@mcgill.ca";
+		
+		try {
+			service.createSession(tutorEmail, date, endTime, startTime);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List<Session> allSessions = service.getAllSessions();
+		
+		assertEquals(1, allSessions.size());
+		assertEquals(date, allSessions.get(0).getDate());
 	}
 
 	@Test
-
+	// Error
 	public void testViewSession() {
+		assertEquals(0, service.getAllSessions().size());
+
+		Calendar c = Calendar.getInstance();
+		c.set(2019, Calendar.NOVEMBER, 01, 14, 00);
+		Date date = new Date(c.getTimeInMillis());
+		Time startTime = new Time(c.getTimeInMillis());
+		c.set(20, 00);
+		Time endTime = new Time(c.getTimeInMillis());
+
+		String tutorEmail = "kathleen@gmail.com";
+		
+		try {
+			service.createSession(tutorEmail, date, endTime, startTime);
+		} catch (IllegalArgumentException e) {
+			// Check that no error occurred
+			fail();
+		}
+
+		List<Session> allSessions = service.getAllSessions();
+
+		assertEquals(1, allSessions.size());
+		assertEquals(startTime, allSessions.get(0).getStarTime());
 
 	}
 
 	@Test
-
-	public void testWriteStudent(){
-
-	}
-	@Test
-
-	public void testViewStudent(){
-
-	}
-
-	@Test
-
+	// Failed
 	public void testWriteSubject(){
+		assertEquals(0, service.getAllSubjects().size());
+
+		String name = "Physics";
+
+		try {
+			service.createSubject(name);
+		} catch (IllegalArgumentException e) {
+			// Check that no error occurred
+			fail();
+		}
+
+		List<Subject> allSubjects = service.getAllSubjects();
+		
+		assertEquals(1, allSubjects.size());
+		assertEquals(name, allSubjects.get(0).getSubjectName());
 
 	}
 
@@ -169,7 +273,24 @@ public class TutoringService321ApplicationTests {
 	@Test
 
 	public void testViewTutor(){
-
+		assertEquals(0, service.getAllTutors().size());
+		
+		String name = "Marwan";
+		String email = "marwan@mcgill.ca";
+		String password = "2";
+		String phoneNumber = "3";
+		int hourlyRate = 5;
+		
+		try {
+			service.createTutor(email, name, password, phoneNumber, hourlyRate);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		List<Tutor> allPersons = service.getAllTutors();
+		
+		assertEquals(1, allPersons.size());
+		assertEquals(phoneNumber, allPersons.get(0).getPhoneNumber());
 	}
 
 
