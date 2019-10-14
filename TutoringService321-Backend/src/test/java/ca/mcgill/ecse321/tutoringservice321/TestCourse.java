30fec4f74ebf0884a675392f54f4af78878a6f49
kyjauna <kyjauna.marshall@mail.mcgill.ca>
2019-10-13 22:34:51 -0400

    Updated TestCourse class, (sorry Katie).

--- TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/TestCourse.java
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/TestCourse.java
@@ -1,8 +1,8 @@
 package ca.mcgill.ecse321.tutoringservice321;
 
-import org.junit.After;
+
 import org.junit.Test;
-import org.junit.Before;
+
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
@@ -14,9 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import ca.mcgill.ecse321.project.model.*;
-import ca.mcgill.ecse321.project.dao.*;
-import ca.mcgill.ecse321.project.service.*;
 import ca.mcgill.ecse321.tutoringservice321.dao.AvailabilityRepository;
 import ca.mcgill.ecse321.tutoringservice321.dao.CourseRepository;
 import ca.mcgill.ecse321.tutoringservice321.dao.SessionRepository;
@@ -45,12 +42,6 @@ public class TestCourse {
 	AvailabilityRepository availabilityRepository;
 	
 	
-	@Before
-	public void setUp(){
-		service.createSubject("Science");
-	}
-	
-	@After
 	public void clearDatabase() {
 		tutorRepository.deleteAll();
 		availabilityRepository.deleteAll();
@@ -63,119 +54,45 @@ public class TestCourse {
 	public void testCreateCourse() {
 
 		String description = "new";
-		String courseCode = "COM SCI 101";
+		String courseCode = "ECSE 321";
 		String id = "1000";
-
-		List<Course> allCourses = service.getAllCourses();
 		
 		try {
+			List<Course> allCourses = service.getAllCourses();
+			
 			service.createCourse(description, courseCode, id);
-			fail();
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
+			
 			assertEquals(1, allCourses.size());
 			assertEquals(id, allCourses.get(0).getCourseID());
 			assertEquals(description, allCourses.get(0).getDescription());
 			assertEquals(courseCode, allCourses.get(0).getCourseCode());
-			assertEquals("McGill", allCourses.get(0).getSchool().getClass());
+			
+		} catch (IllegalArgumentException e) {
+			// Check that no error occurred
 		}
 	}
 	
-	@Test
-	public void testUpdateCourse() {
-
-		String description = "new";
-		String courseCode = "COM SCI 101";
-		String id = "1000";
-	
-		List<Course> allCourses = service.getAllCourses();
-		
-		try {
-			service.createCourse(description, courseCode, id);
-			fail();
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			assertEquals(1, allCourses.size());
-			assertEquals(id, allCourses.get(0).getCourseID());
-			assertEquals(description, allCourses.get(0).getDescription());
-			assertEquals(courseCode, allCourses.get(0).getCourseCode());
-			assertEquals("McGill", allCourses.get(0).getSchool().getClass());
-		}
-
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(1, allCourses.size());
-		assertEquals(id, allCourses.get(0).getCourseID());
-		assertEquals(description, allCourses.get(0).getDescription());
-		assertEquals(courseName, allCourses.get(0).getCourseCode());
-		assertEquals("McGill", allCourses.get(0).getUniversity().getName());
-		
-		description = "updated";
-		courseName = "COM SCI 102";
-		id = 1001;
-		
-		try {
-			service.updateCourse(1000, description, courseName, id, 1);
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			fail();
-		}
-
-		allCourses = service.getAllCourses();
-
-		assertEquals(1, allCourses.size());
-		assertEquals(id, allCourses.get(0).getCourseID());
-		assertEquals(description, allCourses.get(0).getDescription());
-		assertEquals(courseName, allCourses.get(0).getCourseCode());
-		assertEquals("McGill", allCourses.get(0).getUniversity().getName());
-	}
-	
-	@Test
-	public void testDeleteCourse() {
-		assertEquals(0, service.getAllCourses().size());
-
-		String description = "new";
-		String courseName = "COM SCI 101";
-		int id = 1000;
-
-		try {
-			service.createCourse(description, courseName, id, 1);
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			fail();
-		}
-
-		try {
-			service.deleteCourse(id);
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			fail();
-		}
-
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(0, allCourses.size());
-	}
 	
 	@Test
 	public void testCreateCourseNullDescription() {
 
 		String description = null;
-		String courseName = "COM SCI 101";
-		int id = 1000;
+		String courseCode = "ECSE 321";
+		String id = "1000";
 		
-		String error = null;
 
 		try {
-			service.createCourse(description, courseName, id, 1);
+			List<Course> allCourses = service.getAllCourses();
+			
+			service.createCourse(description, courseCode, id);
+			
+			assertEquals(0, allCourses.size());
+			
 		} catch (IllegalArgumentException e) {
 			// Check that no error occurred
-			error = e.getMessage();
+			fail();
 		}
 
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(0, allCourses.size());
 
 	}
 	
@@ -183,21 +100,22 @@ public class TestCourse {
 	public void testCreateCourseNullCourseName() {
 
 		String description = "new";
-		String courseName = null;
-		int id = 1000;
+		String courseCode = null;
+		String id = "1000";
 
-		String error = null;
 		
 		try {
-			service.createCourse(description, courseName, id, 1);
+		
+			List<Course> allCourses = service.getAllCourses();
+			
+			service.createCourse(description, courseCode, id);
+			
+			assertEquals(0, allCourses.size());
+			
 		} catch (IllegalArgumentException e) {
 			// Check that no error occurred
-			error = e.getMessage();
+			fail();
 		}
-
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(0, allCourses.size());
 
 	}
 	
@@ -205,45 +123,21 @@ public class TestCourse {
 	public void testCreateCourseNullID() {
 
 		String description = "new";
-		String courseName = "COM SCI 101";
-		int id = (Integer)null;
+		String courseCode = "COM SCI 101";
+		String id = null;
 		
-		String error = null;
 
 		try {
-			service.createCourse(description, courseName, id, 1);
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			error = e.getMessage();
+			List<Course> allCourses = service.getAllCourses();
+			
+			service.createCourse(description, courseCode, id);
+			
+			assertEquals(0, allCourses.size());
+			
+		} catch (IllegalArgumentException e){
+			fail();
 		}
-
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(0, allCourses.size());
-
-	}
-
-	@Test
-	public void testCreateCourseNoUniversity() {
-
-		String description = "new";
-		String courseName = "COM SCI 101";
-		int id = 1000;
-		
-		String error = null;
-
-		try {
-			service.createCourse(description, courseName, id, 2);
-		} catch (IllegalArgumentException e) {
-			// Check that no error occurred
-			error = e.getMessage();
-		}
-
-		List<Course> allCourses = service.getAllCourses();
-
-		assertEquals(0, allCourses.size());
 
 	}
 }
 
-}
