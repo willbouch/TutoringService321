9c6a028f109b757deb77dd6741da4a64ac73fad3
willbouch <williambouchardpei@gmail.com>
2019-10-18 13:49:26 -0400

    Setup for Sprint 2 : Ready to create branches for different features

---
+++ TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/controller/TutoringService321RestController.java
@@ -0,0 +1,18 @@
+package ca.mcgill.ecse321.tutoringservice321.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ca.mcgill.ecse321.tutoringservice321.service.TutoringService321Service;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class TutoringService321RestController {
+	
+	@Autowired
+	private TutoringService321Service service;
+
+}
