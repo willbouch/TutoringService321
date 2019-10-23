6236649a49ef6798fe90e2dcb4d3965020f6e5d2
kehwhy <kyounge2107@outlook.com>
2019-10-23 16:59:27 -0400

    replaced TutoringService321ServiceTests with servicetests suite

---
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/servicetests/CourseServiceTests.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse321.tutoringservice321.servicetests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CourseServiceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/servicetests/SessionServiceTests.java
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/servicetests/SessionServiceTests.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.tutoringservice321;
+package ca.mcgill.ecse321.tutoringservice321.servicetests;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,11 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TutoringService321ServiceTests {
+public class SessionServiceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
