08fd28e20ee2befbedf74604e6776ccd00f91d63
hadizia <hadizia6@gmail.com>
2019-10-24 17:18:47 -0400

    check it please will

--- TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/controller/TutoringService321RestController.java
+++ TutoringService321-Backend/src/main/java/ca/mcgill/ecse321/tutoringservice321/controller/TutoringService321RestController.java
@@ -50,7 +50,7 @@ public class TutoringService321RestController {
 			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern = "HH:mm") LocalTime endTime) {
 
 		Availability availability = service.addAvailability(tutorEmail, date, Time.valueOf(startTime), Time.valueOf(endTime));
-
+//checking
 		return converToDto(availability);
 	}
 
@@ -60,10 +60,9 @@ public class TutoringService321RestController {
 		for(Availability availability : service.getAllTutorAvailabilities(tutorEmail)) {
 			dtos.add(converToDto(availability));
 		}
-
 		return dtos;
 	}
-
+	
 	@DeleteMapping(value = {"/availabilities/{tutorEmail}", "/availabilities/{tutorEmail}/"})
 	public List<AvailabilityDto> deleteAvailability(@PathVariable("tutorEmail") String tutorEmail,
 			@RequestParam Date date,
--- TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/servicetests/SubjectServiceTests.java
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/servicetests/SubjectServiceTests.java
@@ -9,6 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import ca.mcgill.ecse321.tutoringservice321.controller.TutoringService321RestController;
+import ca.mcgill.ecse321.tutoringservice321.model.Subject;
+import ca.mcgill.ecse321.tutoringservice321.service.TutoringService321Service;
+import ca.mcgill.ecse321.tutoringservice321.dao.*;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -19,10 +24,60 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SubjectServiceTests {
+	
+	@Mock
+	private SubjectRepository subjectDao;
+
+	@InjectMocks
+	private TutoringService321Service service;
+	
+	private static final String SUBJECT_KEY = "TestPerson";
+	//private static final String NONEXISTING_KEY = "NotAPerson";
+
+	
+	@Before
+	public void setMockOutput() {
+		when(subjectDao.findSubjectBySubjectName(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+			if(invocation.getArgument(0).equals(SUBJECT_KEY)) {
+				Subject subject = new Subject();
+				subject.setSubjectName(SUBJECT_KEY);
+				return subject;
+			} else {
+				return null;
+			}
+		});
+	}
+   
+	private Subject subject;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateSubject() {
+		assertEquals(0, service.getAllSubjects().size());
+		
+		String name = "Mathematics";
+		
+		try {
+			 subject = service.createSubject(name);
+		}catch (IllegalArgumentException e) {
+			fail();
+		}
+		
+		assertEquals(name,subject.getSubjectName());
+		
+	}
+	
+	
+	@Test
+	public void testCreateSubjectNull() {
+		String name= null;
+		String error= null;
+		
+		try {
+			 subject = service.createSubject(name);
+		}catch(IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		assertEquals("The name cannot be empty.", error );
 	}
 
 }
