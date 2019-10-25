5bc5d708a7e8c5947d5b5c45dad4888ba808ff27
kehwhy <kyounge2107@outlook.com>
2019-10-25 15:41:57 -0400

    setup for controllertests complete

---
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/controllertests/CourseControllerTests.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.tutoringservice321.controllertests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CourseControllerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/persistencetests/TutoringService321ApplicationTests.java
+++ TutoringService321-Backend/src/test/java/ca/mcgill/ecse321/tutoringservice321/persistencetests/TutoringService321ApplicationTests.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.tutoringservice321;
+package ca.mcgill.ecse321.tutoringservice321.persistencetests;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
