b8afbbf53e939504691672cd508f4a4882eeb461
Kyjauna <kyjauna.marshall@mail.mcgill.ca>
2019-11-10 23:44:08 -0500

    bare bones of .vue components - they are empty - imports from bootstrap

--- tutoringservice-frontend/src/router/index.js
+++ tutoringservice-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import TutoringService from '@/components/TutoringService'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'TutoringService',
+      component: TutoringService
     }
   ]
 })
