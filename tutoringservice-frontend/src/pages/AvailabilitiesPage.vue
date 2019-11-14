bc43fc980ccf62e2b5f0b6d63a28123b52e5ef4d
kehwhy <kyounge2107@outlook.com>
2019-11-14 02:05:26 -0500

    got toggle workinggit add .

--- tutoringservice-frontend/src/pages/AvailabilitiesPage.vue
+++ tutoringservice-frontend/src/pages/AvailabilitiesPage.vue
@@ -1,25 +1,213 @@
 <template>
   <div id="AvailabilitiesPage">
-	  	<h1>Availabilities</h1>
-  <div id="weekly-availabilities"> Weekly Availability</div>
-		<AvailabilityBox></AvailabilityBox>
+	  	<h1 id="header">Availabilities</h1>
+  <div id="weekly-availabilities"></div>
+		<table id="schedule" style="width:100%">
+			<col id="time" width="80">
+			<col width="140" id="monday">
+			<col width="140" id="tuesday">
+			<col width="140" id="wednesday">
+			<col width="140" id="thursday">
+			<col width="140" id="friday">
+			<col width="140" id="saturday">
+			<col width="140" id="sunday">
+
+			<tr id="days">
+				<th> </th>
+				<th> Monday </th>
+				<th> Tuesday </th>
+				<th> Wednesday </th>
+				<th> Thursday </th>
+				<th> Friday </th>
+				<th> Saturday </th>
+				<th> Sunday </th>
+			</tr>
+			<tr id="9am">
+				<th align="right"> 9:00 am </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="10am">
+				<th align="right"> 10:00 am </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="11am">
+				<th align="right"> 11:00 am </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="12pm">
+				<th align="right"> 12:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="1pm">
+				<th align="right"> 1:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="2pm">
+				<th align="right"> 2:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="3pm">
+				<th align="right"> 3:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="4pm">
+				<th align="right"> 4:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="5pm">
+				<th align="right"> 5:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="6pm">
+				<th align="right"> 6:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="7pm">
+				<th align="right"> 7:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="8pm">
+				<th align="right"> 8:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			<tr id="9pm">
+				<th align="right"> 9:00 pm </th>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+				<td class='notavailable' v-on:click="toggle"></td>
+			</tr>
+			</table>
+			<button class="button" v-on:click=setAvailability> Set Availability </button>
+			<button class="button" v-on:click="goToMainPage"> Back to Profile </button>
 	</div>
 </template>
 
 <script>
+function availabilityDto (day, startTime, endTime) {
+	this.day = day
+	this.startTime = startTime
+	this.endTime = endTime
+}
+
 export default {
-	name: 'AvailabilitiesPage',
-	components:'AvailabilityBox', 
+	name: 'AvailabilitiesPage', 
+	  
+ data() {
+    return {
+      availabilities: []
+		}
+	},
+	
+	created: function() {
+		const a1 = new availabilityDto("Monday", '10:00:00', '12:00:00')
+		const a2 = new availabilityDto("Friday", '10:00:00', '12:00:00')
+		const a3 = new availabilityDto("Thursday", '10:00:00', '12:00:00')
+
+		this.availabilities = [a1,a2,a3]
+	},
 	methods: {
 		goToMainPage(){
 			this.$router.push('MainPage')
+		},
+
+ 		toggle(event){
+			 var cell=event.target;			 
+					 if (cell.className == "available")
+						 cell.className = "notavailable";
+						else if(cell.className == "notavailable")
+							cell.className = "available";
+		},
+
+		setAvailability(){
+			var availabilities = table.getElementsByClassName("available");
+			var i; 
+			for (i=0; i<availabilities.length; i++){
+
+			}
 		}
-	}
+		}
 }
 </script>
 
 <style>
-#AvailabilityPage {
+
+#AvailabilitiesPage {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -27,4 +215,46 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+/* Table */
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+
+th, td {
+  text-align: center;
+	height: 10px;
+  padding: 10px;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+button {
+  background-color: #ddd;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 6px 4px;
+  cursor: pointer;
+  border-radius: 16px;
+  top: 0px;
+}
+
+.available{
+	background-color: greenyellow;
+}
+
+.notavailable{
+	background-color: #ff6961;
+}
+
 </style>
