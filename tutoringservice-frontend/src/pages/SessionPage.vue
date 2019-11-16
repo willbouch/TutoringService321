81611d09ef82a0a275b943c6140db18881d89ddb
Kyjauna <kyjauna.marshall@mail.mcgill.ca>
2019-11-16 16:00:20 -0500

    yeehaw

--- tutoringservice-frontend/src/pages/SessionPage.vue
+++ tutoringservice-frontend/src/pages/SessionPage.vue
@@ -23,16 +23,15 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr v-for="Session in Sessions" :key="Session">
+				<tr :class="$data._rowVarient" v-for="Session in cptItems" :key="Session">
 					<td>{{Session.student}}</td>
 					<td>{{Session.time}}</td>
 					<td>{{Session.date}}</td>
 					<td>{{Session.room}}</td>
 					<td>{{Session.email}}</td>
 					<td>{{Session.course}}</td>
-					<td><button :class='isApproved' @click="approvedClass(Session)" class="btn btn-success">Approve</button></td>
-					<td><button :disabled='isEnabled' @click="declinedClass(Session)" class="btn btn-danger" >Decline</button></td>
-					
+					<td><button  :disabled='isEnabled' @click="approvedClass(Session)" class="btn btn-success">Approve</button></td>
+					<td><button  @click="declineClass(Session)" class="btn btn-danger" >Decline</button></td>
 				</tr>
 			</tbody>
 		</table>
@@ -47,20 +46,25 @@ export default {
   data : function()  {
 	  return{
 			Sessions,
-			_rowVariant: 'danger'
+			approved: false,
+			_rowVarient: this.variant
 	  }
 	},
+	
 	computed: {
-  	isDisabled: function(){
-    	return !this.approved;
+  	isEnabled: function(){
+    	return this.approved;
 		},
-		Enabled: function(){
-			return this.approved;
-		},
-		isApproved: function(){
-			return this._rowVarient
-		}
-		
+     cptItems(){
+        return this.Sessions.map((Sessions)=>{
+							 let tmp=Sessions;
+							 _rowVarient:{
+								Sessions.Session==this.approved?tmp.variant='warning':tmp.variant='success';
+							 }
+                return tmp;
+
+        })  
+        }
 	},
 	methods: {
   	declineClass(Session) {
@@ -70,18 +74,16 @@ export default {
 				}
 			}
 		},
-		approvedClass(Session) {
-			console.log(Session)
-			return this._rowVariant
 		
-	},
-		cancelClass(Session) {
+		approvedClass(Session) {
 			for(var i = 0; i < this.Sessions.length; i++){
 				if(Sessions[i].student == Session.student){
-					Sessions.splice(i,1)
-				}
-			}
-		},
+				approved: true 
+				this.class="approved";
+		   console.log(Session);
+		    }
+	    }
+	  }
 	}
 }
 
@@ -99,8 +101,12 @@ export default {
   cursor: pointer;
 }
 	.approved {
-		background-color: lightyellow,
+		background-color: #f2f2f2;
 }
+	tr.approved td{
+		background-color: lightyellow;
+	}
+
   .tab {
   overflow: hidden;
   border: 1px solid #ccc;
